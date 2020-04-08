ae1a47c4a07314d7d8282078c7d687c860f235b2
srinivasa <srinivasavy@gmail.com>
2020-04-08 15:38:53 +0530

    this is update with akshmi

---
+++ DBPractice/src/myTestwithlakshmi.java
@@ -0,0 +1,11 @@
+
+public class myTestwithlakshmi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is update with lakshmi");
+		System.out.println("we will see in local OR");
+
+	}
+
+}
