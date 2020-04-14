ec53fb2fa3262297faea61a7df3c27a736fa77a6
srinivasa <srinivasavy@gmail.com>
2020-04-14 14:08:41 +0530

    this is integration between git and jenkins

---
+++ DBPractice/src/testWithGitJenkinsIntegrations.java
@@ -0,0 +1,13 @@
+
+public class testWithGitJenkinsIntegrations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is sample from git and jenkins ");
+		System.out.println("this is update from eclipse to git");
+		System.out.println("this is update from git to jenkins");
+		System.out.println("it's worked properly");
+
+	}
+
+}
