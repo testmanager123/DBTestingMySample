c7fa9b3b66c1bc86951ac2fd7e32096b0421fc00
srinivasa <srinivasavy@gmail.com>
2020-04-16 12:27:39 +0530

    jenkins executions

--- DBPractice/src/myupdatTest.java
+++ DBPractice/src/myupdatTest.java
@@ -4,6 +4,7 @@ public class myupdatTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my udapte checking");
+		System.out.println("execute from jenkins");
 
 	}
 
